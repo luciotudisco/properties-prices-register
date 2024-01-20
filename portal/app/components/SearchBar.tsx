a08226ae85c128d6a3647bcb462ebed0bf90c4a4
Lucio Tudisco <lucio.tudisco@gmail.com>
2024-01-20 14:50:01 +0000

    Responsive search bar

--- portal/app/components/SearchBar.tsx
+++ portal/app/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Button, Drawer } from "@mui/material";
+import { Badge, Box, Button, Drawer, IconButton } from "@mui/material";
 import {
   SearchBox,
   useCurrentRefinements,
@@ -8,6 +8,8 @@ import {
 import "instantsearch.css/themes/satellite.css";
 import { useState } from "react";
 import SearchFilters from "./SearchFilters";
+import TuneIcon from "@mui/icons-material/Tune";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const SearchBar = function (): JSX.Element {
   const [showFilters, setShowFilters] = useState<boolean>(false);
@@ -35,16 +37,36 @@ const SearchBar = function (): JSX.Element {
   }
 
   return (
-    <Box className="flex flex-row w-full border-b-2 p-5 gap-2 bg-emerald-950">
+    <Box className="flex flex-row w-full border-b-2 p-5 gap-2 bg-emerald-950 items-center">
       <SearchBox className="w-full" placeholder="Search property" />
       <Badge badgeContent={items.length} color="error">
-        <Button onClick={() => openFilters()} className="text-white">
+        <Button
+          onClick={() => openFilters()}
+          className="text-white hidden md:block"
+        >
           Filters
         </Button>
+        <IconButton
+          onClick={() => openFilters()}
+          className="text-white block md:hidden"
+          aria-label="Open Filters"
+        >
+          <TuneIcon />
+        </IconButton>
       </Badge>
-      <Button onClick={() => clearFilters()} className="text-white">
+      <Button
+        onClick={() => clearFilters()}
+        className="text-white hidden md:block"
+      >
         Clear
       </Button>
+      <IconButton
+        onClick={() => clearFilters()}
+        className="text-white block md:hidden"
+        aria-label="Clear Filters"
+      >
+        <ClearIcon />
+      </IconButton>
       <Drawer
         anchor="right"
         open={showFilters}
