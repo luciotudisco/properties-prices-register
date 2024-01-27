3159312c0071805a7c81e277a67e9506e537e27a
luciotudisco <lucio.tudisco@gmail.com>
2024-01-27 11:07:24 +0000

    Update libs

--- service/ppr/celery.py
+++ service/ppr/celery.py
@@ -11,7 +11,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     "load_properties": {
         "task": "load_properties",
-        "schedule": crontab(hour=22),  # every day at 22:00 UTC
+        "schedule": crontab(hour=12),  # every day at 12:00 UTC
         "args": [3],  # Load properties sold in the past 3 months
     }
 }
